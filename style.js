5541a8b88d72c5d7d71be6ea97d4451f2cd38375
jabbran-malikgit config --global user.email jabbranishtiaq.10@gmail.com <jabbranishtiaq.10@gmail.com>
2025-10-16 02:07:02 +0500

    Add initial Files

---
+++ style.js
@@ -0,0 +1,33 @@
+const toggler=document.querySelector('.hamburger');
+const navpages=document.querySelector('.nav-pages');
+
+toggler.addEventListener('click', ()=>{
+    navpages.classList.toggle('active');
+}) 
+
+// const cardsdata=[
+//     {
+//     image:"",
+//     heading:'Tonic',
+//     tags: ["HTML", "CSS", "Javascript"],
+//     button:'See project'
+// }
+//     {
+//     image:"",
+//     heading:'Tonic',
+//     tags: ["HTML", "CSS", "Javascript"],
+//     button:'See project'
+// }
+//     {
+//     image:"",
+//     heading:'Tonic',
+//     tags: ["HTML", "CSS", "Javascript"],
+//     button:'See project'
+// }
+//     {
+//     image:"",
+//     heading:'Tonic',
+//     tags: ["HTML", "CSS", "Javascript"],
+//     button:'See project'
+// }
+// ]
