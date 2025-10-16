d01e20bc108d5eeb3920909652308e0c2142c304
jabbran-malikgit config --global user.email jabbranishtiaq.10@gmail.com <jabbranishtiaq.10@gmail.com>
2025-10-17 03:03:11 +0500

    My latest code updates

--- style.js
+++ style.js
@@ -4,30 +4,3 @@ const navpages=document.querySelector('.nav-pages');
 toggler.addEventListener('click', ()=>{
     navpages.classList.toggle('active');
 }) 
-
-// const cardsdata=[
-//     {
-//     image:"",
-//     heading:'Tonic',
-//     tags: ["HTML", "CSS", "Javascript"],
-//     button:'See project'
-// }
-//     {
-//     image:"",
-//     heading:'Tonic',
-//     tags: ["HTML", "CSS", "Javascript"],
-//     button:'See project'
-// }
-//     {
-//     image:"",
-//     heading:'Tonic',
-//     tags: ["HTML", "CSS", "Javascript"],
-//     button:'See project'
-// }
-//     {
-//     image:"",
-//     heading:'Tonic',
-//     tags: ["HTML", "CSS", "Javascript"],
-//     button:'See project'
-// }
-// ]
